5c2be80b8753b8bea44994f73371d59471fb5678
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-11-07 21:58:49 -0300

    Iniciação do projeto

---
+++ Projeto ToDo/Código-fonte/todo (dentro htdocs)/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo</title>
+    <link href="css/estilo.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/69e5255fc0.js" crossorigin="anonymous"></script>
+    <link href="img/logo.png" rel="icon" type="image/x-icon">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand text-white" href="#">
+                <img src="img/logo.png" width="30" height="30" alt="logo">
+                SuperToDo
+            </a>
+        </div>
+    </nav>
+
+</body>
+
+</html>
---
+++ Projeto ToDo/Código-fonte/todo_scripts (fora htdocs)/conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+    //PARAMOS AQUI :P
+
+?>
